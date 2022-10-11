8beb3a908df71360efcce8612e17bec20dfe7f78
kenkso <ken@hkconsulting.biz>
2022-10-11 15:00:57 -0600

    new template

--- src/components/flag/index.tsx
+++
@@ -1,6 +0,0 @@
-export type FlagProps = {size: string; code: string};
-const Flag: React.FC<FlagProps> = ({size = "lg", code}) => {
-  return <span className={`text-${size} flag-icon flag-icon-${code}`}></span>;
-};
-
-export default Flag;
